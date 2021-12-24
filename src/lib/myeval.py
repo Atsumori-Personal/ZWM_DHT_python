f19e8cce626e18510fd4965b4b2f51ad28993fba
Atsumori-Personal <d.roseofpain777@gmail.com>
2021-12-24 10:50:29 +0900

    Fixed BER-func.

--- src/lib/myeval.py
+++ src/lib/myeval.py
@@ -4,11 +4,11 @@ def ber(a, b):
     Height = a.shape[0]
     Width = a.shape[1]
 
-    correct = 0
+    wrong = 0
 
     for i in range(Height):
         for j in range(Width):
-            if a[i,j] == b[i,j]:
-                correct += 1
+            if a[i,j] != b[i,j]:
+                wrong += 1
     
-    return correct / (Height * Width)
+    return wrong / (Height * Width)
